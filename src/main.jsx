542782a62034f1ea232d7e69d98484c67dbad271
Rashad Edwards <rashadedwards@macbook.local>
2024-10-29 22:01:43 -0400

    added about page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "../src/styles/navBar.css";
 import "../src/styles/titlePage.css";
 import "../src/styles/homePage.css";
-import "../src/styles/navBar.css";
+import "../src/styles/aboutPage.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,7 +5,44 @@ import PageTemplate from "../components/PageTemplate";
 function AboutPage() {
   return (
     <PageTemplate>
-      <h1 className="home-title">About</h1>
+      <div className="about-page-container">
+        <h1 className="about-title">Discover Your Food’s Story</h1>
+        <p id="about-page-paragraph">
+          iNibble gives you insights into the food you eat, helping you
+          understand how each nibble can nourish and protect you. Make informed
+          choices and start each day with the confidence that what you eat
+          supports a healthier, more vibrant life.
+        </p>
+
+        <section className="about-section">
+          <h2 className="about-section-title">Empowering Healthy Choices</h2>
+          <p className="about-section-paragraph">
+            Our goal is to make eating a mindful and rewarding experience. We
+            provide clear, easily accessible information about how food impacts
+            your body, from boosting energy to fortifying health. Join us on a
+            journey toward more conscious, health-focused eating.
+          </p>
+        </section>
+
+        <section className="about-section">
+          <h2 className="about-section-title">How iNibble Works</h2>
+          <p className="about-section-paragraph">
+            With data-driven insights and a personalized touch, iNibble allows
+            you to track what you eat and how it affects your well-being. Our
+            platform is designed to educate and empower, putting nutritional
+            knowledge at your fingertips.
+          </p>
+        </section>
+
+        <section className="about-section">
+          <h2 className="about-section-title">Your Health, Our Priority</h2>
+          <p className="about-section-paragraph">
+            We’re committed to helping you make choices that contribute to a
+            balanced lifestyle. At iNibble, we believe that food is a key to
+            wellness, and we’re here to support you every step of the way.
+          </p>
+        </section>
+      </div>
     </PageTemplate>
   );
 }
